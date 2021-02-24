05414c24d0483ca91886e690b463c7ffd7bba199
ugoswami11 <66896876+ugoswami11@users.noreply.github.com>
2021-02-24 10:38:22 +0530

    Add files via upload

---
+++ MavenProjectTesting/src/main/java/com/sample/App.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{	
+	@Test
+    public  void Main()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
